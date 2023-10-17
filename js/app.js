33a5040142fb6d38c99a796df1c7bbe0bc4fca8e
Hugo Barrera <exailer89@gmail.com>
2023-10-16 21:29:58 -0600

    Validando el Formulario para leer contenido escrito en textarea y agregar alerta en caso de estar vacio

--- js/app.js
+++ js/app.js
@@ -15,5 +15,30 @@ function eventListeners() {
 // Funciones
 function agregarTweet(e) {
     e.preventDefault();
-    console.log('Agregando Tweet');
+    
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+    
+    // Validacion del campo
+    if (tweet === '') {
+        mostrarError('Este campos no puede ir vacio');
+        return;
+    }
+
+    console.log(tweet);
+}
+
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertar el mensaje en el contenido
+    const contenido = document.querySelector('#contenedor-formulario');
+    contenido.appendChild(mensajeError);
+
+    // Elimina la alerta despues de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000)
 }
