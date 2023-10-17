dc0a9a384247805be027676a39c95d190f01a787
Hugo Barrera <exailer89@gmail.com>
2023-10-16 21:08:45 -0600

    Iniciando el proyecto - Se crean variables, eventos y funciones inciales

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+
+// Eventos
+eventListeners();
+
+function eventListeners() {
+    formulario.addEventListener('submit', agregarTweet);
+}
+
+
+// Funciones
+function agregarTweet(e) {
+    e.preventDefault();
+    console.log('Agregando Tweet');
+}
